bad7bcdaafeaaa76c3a7db302e749ab4f71feed6
Feroz Zargar <Ferozzargar@hotmail.com>
2022-12-13 00:27:12 -0500

    Important imports added to App.js

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,18 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CaseBaseline, CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import { Route, Routes } from "react-router-dom";
+import { Sidebar } from "./scenes/global/Sidebar";
+import { Dashboard } from "./scenes/dashboard";
+import { Team } from "./scenes/team";
+import { Invoices } from "./scenes/invoices";
+import { Contacts } from "./scenes/contacts";
+import { Bar } from "./scenes/bar";
+import { Form } from "./scenes/form";
+import { Line } from "./scenes/line";
+import { Pie } from "./scenes/pie";
+import { FAQ } from "./scenes/faq";
+import { Geography } from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +21,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" elemet={<Dashboard />} />
+            </Routes>
+          </main>
         </div>
         ;
       </ThemeProvider>
