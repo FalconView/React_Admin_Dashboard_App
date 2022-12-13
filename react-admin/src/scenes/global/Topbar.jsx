7fc361bc4f935fec92db1c23e88f2402a285a3c3
Feroz Zargar <Ferozzargar@hotmail.com>
2022-12-12 23:38:09 -0500

    Search bar was styled in Topbar.jsx step 3

--- react-admin/src/scenes/global/Topbar.jsx
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material";
+import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -26,9 +26,30 @@ const Topbar = () => {
         <IconButton></IconButton>
       </Box>
 
-      <IconButton>
+      <IconButton type="button" sx={{ p: 1 }}>
         <SearchIcon />
       </IconButton>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+          <LightModeOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
