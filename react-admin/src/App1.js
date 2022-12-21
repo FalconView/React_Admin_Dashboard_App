9c654839b112b7b9e570dc4c7bcb7e8cb3288476
Feroz Zargar <Ferozzargar@hotmail.com>
2022-12-20 22:15:10 -0500

    Sidebar.jsx was updated stage 3

--- react-admin/src/App1.js
+++ react-admin/src/App1.js
@@ -24,20 +24,6 @@ function App() {
                  */}
                 {/* <Route path="/invoices" elemet={<Invoices />} />
                  */}
-                {/* <Route path="/form" elemet={<Form />} />
-                 */}
-                {/* <Route path="/bar" elemet={<Bar />} />
-                 */}
-                {/* <Route path="/pie" elemet={<Pie />} />
-                 */}
-                {/* <Route path="/line" elemet={<Line />} />
-                 */}
-                {/* <Route path="/faq" elemet={<FAQ />} />
-                 */}
-                {/* <Route path="/geography" elemet={<Geography />} />
-                 */}
-                {/* <Route path="/calendar" elemet={<Calendar />} />
-                 */}
               </Routes>
             </Routes>
           </main>
